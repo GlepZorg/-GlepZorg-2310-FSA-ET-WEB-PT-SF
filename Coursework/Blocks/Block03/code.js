c4959f8730db2e815a256546f29ad7d5b7c07e6e
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2023-10-24 08:57:12 -0700

    Intial Commit

---
+++ Coursework/Blocks/Block03/code.js
@@ -0,0 +1,1 @@
+console.log("Hello Alexis Gonzalez!")
---
+++ Coursework/Guided_Practice/Block07/main.js
@@ -0,0 +1,9 @@
+let message = "Hello World!";
+let message2 = "Disney Land!";
+console.log(message+ " " + message2);
+let numCows = 50;
+console.log(numCows)
+let numOfUmbrellas = 10 * 3;
+console.log(numOfUmbrellas)
+let  num = 5 + 1;
+console.log(num);
